6a800bdeb0230d130d0c0bc71ff3a3e8f8784a2e
moonShadow <331614794@qq.com>
2020-03-02 05:02:23 +0800

    v0.0.1

--- podFuck.sh
+++ podFuck.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+export MOON_FILE=Fuckfile
+export MOON_PATH=~/.moonPods
+
+export MOON_SERVER=47.74.8.253
+export MOON_SERVER_PASSWORD="moongit"
+
 showMenu(){
     echo "====== 你要搞咩？======"
-    echo "1> remove trunk - 删除 .cocoaPods 文件夹下的 trunk 目录"
+    echo "1> remove trunk - 删除 trunk 目录"
     echo "2> change repo  - 更换 repo 源为镜像源"
     echo "3> TODO: setup git proxy"
-    echo "4> TODO: feach & download gits"
+    echo "4> moon gits    - 海外服务器下载相应仓库，本机再去拉取，详见 README.md 说明"
+    echo "5> help"
     echo "0> exit"
 }
 
@@ -96,6 +103,35 @@ gitProxys(){
     done
 }
 
+fuckGits(){
+    if [ ! -d ${MOON_PATH} ]; then
+        echo "正在创建 ~/.moonPods 文件夹，可能需要输入您的管理员密码..."
+        mkdir -p ${MOON_PATH}
+    fi
+    cp ./Fuckfile ${MOON_PATH}/Fuckfile
+    scp ./Fuckfile git@${MOON_SERVER}:/git/Fuckfile
+    ssh -o StrictHostKeyChecking=no git@${MOON_SERVER} <<eeooff
+    cd /git
+    ./updateGits.sh
+    exit
+eeooff
+    echo "东京服务器仓库下载完成..."
+    cd ${MOON_PATH}
+    rm ${MOON_PATH}/Podfile
+    while read line
+    do
+        resName=${line##*/}
+        simpleName=${resName%%.git*}
+        git clone git@${MOON_SERVER}:/git/${simpleName}
+        # pod 'AFNetworking', :path => '~/Documents/AFNetworking', :branch => 'dev', :tag => '1.0.0'
+        echo "pod '${simpleName}', :path => '~/.moonPods/${simpleName}'" >> Podfile
+    done < ${MOON_PATH}/Fuckfile
+    rm ${MOON_PATH}/Fuckfile
+    echo "#本文件路径为 ${MOON_PATH}/Podfile ，方便用户替换工程中 Podfile 的相应内容，输入 :wq 退出..." >> Podfile
+    vi ${MOON_PATH}/Podfile
+    cd -
+}
+
 showMenu
 while read -p "podFuck>>>" idx
 do
@@ -111,6 +147,12 @@ do
     elif [[ ${idx} == "3" ]]
     then
         gitProxys
+    elif [[ ${idx} == "4" ]]
+    then
+        fuckGits
+    elif [[ ${idx} == "5" ]]
+    then
+        open README.md
     else
         showMenu
     fi
