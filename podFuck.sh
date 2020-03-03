1460d1f06b7d196964a39a0940be19e124066a0c
月之暗面 <xuyiding@xiaomai5.com>
2020-03-03 19:18:15 +0800

    tmp

--- podFuck.sh
+++ podFuck.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-MOON_FILE=Fuckfile
 MOON_PATH=~/.moonPods
+MOON_FILE=MOONFile
 
 MOON_SERVER=47.74.8.253
-MOON_SERVER_PASSWORD="moongit"
-
-testPath=~/.moonTest
+MOON_SERVER_PASSWORD=moongit
 
 showMenu() {
     echo "====== 你要搞咩？======"
@@ -109,36 +107,71 @@ gitProxys() {
     done
 }
 
-fuckGits() {
+serverUpdate() {
+    if [[ -x /usr/bin/expect ]]; then
+        echo "expect exist..."
+        /usr/bin/expect <<EOF
+        spawn scp ./${MOON_FILE} git@${MOON_SERVER}:/git/${MOON_FILE}
+        expect "password:"
+        send "${MOON_SERVER_PASSWORD}\r"
+        expect eof
+EOF
+        /usr/bin/expect <<EOF
+        spawn ssh -o StrictHostKeyChecking=no git@${MOON_SERVER} "cd /git; ./updateGits.sh ${MOON_FILE}; exit;"
+        expect "password:"
+        send "${MOON_SERVER_PASSWORD}\r"
+        expect eof
+EOF
+        echo "东京服务器仓库下载完成..."
+        return 0
+    else
+        echo "未安装 expect "
+        return 1
+    fi
+}
+
+checkFiles() {
     if [ ! -d ${MOON_PATH} ]; then
-        echo "正在创建 ~/.moonPods 文件夹，可能需要输入您的管理员密码..."
+        echo "正在创建 ~/.moonPods 文件夹..."
         mkdir -p ${MOON_PATH}
     fi
-    cp ./Fuckfile ${MOON_PATH}/Fuckfile
-    scp ./Fuckfile git@${MOON_SERVER}:/git/Fuckfile
-    ssh -o StrictHostKeyChecking=no git@${MOON_SERVER} <<eeooff
-    cd /git
-    ./updateGits.sh
-    exit
-eeooff
-    echo "东京服务器仓库下载完成..."
-    if [[ -d ${MOON_PATH} ]]; then
+    if [ ! -d ${MOON_PATH} ]; then
+        echo "${MOON_PATH} 可能创建失败，请以管理员身份运行脚本..."
+        return 1
+    else
+        return 0
+    fi
+}
+
+fuckGits() {
+    serverUpdate
+    if checkFiles; then
+        cp ./${MOON_FILE} ${MOON_PATH}/${MOON_FILE}
         cd ${MOON_PATH}
         rm ${MOON_PATH}/Podfile
-        while read line; do
+        echo "${MOON_PATH}/${MOON_FILE}"
+        cat ${MOON_PATH}/${MOON_FILE} | while read line; do
             resName=${line##*/}
             simpleName=${resName%%.git*}
             git clone git@${MOON_SERVER}:/git/${simpleName}
-            # pod 'AFNetworking', :path => '~/Documents/AFNetworking', :branch => 'dev', :tag => '1.0.0'
-            echo "pod '${simpleName}', :path => '~/.moonPods/${simpleName}'" >>Podfile
-        done <${MOON_PATH}/Fuckfile
-        rm ${MOON_PATH}/Fuckfile
-        echo "#本文件路径为 ${MOON_PATH}/Podfile ，方便用户替换工程中 Podfile 的相应内容，输入 :wq 退出..." >>Podfile
+            echo "pod '${simpleName}', :path => '~/.moonPods/${simpleName}', :branch => 'master'" >>Podfile
+        done
+        rm ${MOON_PATH}/${MOON_FILE}
+        echo "#本文件路径为 ${MOON_PATH}/Podfile ，方便用户替换工程中 Podfile 的相应内容..." >>Podfile
         vi ${MOON_PATH}/Podfile
         cd -
-    else
-        echo "${MOON_PATH} 可能创建失败，请以管理员身份运行脚本"
     fi
+}
+
+test() {
+    cd ~/.moonPods
+    git clone https://github.com/darkThanBlack/MOONAssistiveTouch
+    sleep 2
+    /usr/bin/expect <<EOF
+    expect "password:"
+    send "moongit\r"
+    expect eof
+EOF
 }
 
 showMenu
@@ -154,7 +187,8 @@ while read -p "podFuck>>>" idx; do
     elif [[ ${idx} == "4" ]]; then
         fuckGits
     elif [[ ${idx} == "5" ]]; then
-        open README.md
+        test
+        # open README.md
     else
         showMenu
     fi
